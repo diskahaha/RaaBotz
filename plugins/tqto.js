380af8f189973c1fde2b26b9a2cfb9243a3d2a72
diskahaha <109210020+diskahaha@users.noreply.github.com>
2022-09-04 22:34:58 +0700

    Update tqto.js

--- plugins/tqto.js
+++ plugins/tqto.js
@@ -14,14 +14,7 @@ Terimakasih Juga Untuk Kang Banned:
 ➸ @${'0'.split('@')[0]}
 
 ──────────────────────
-• God
-• My ortu
-• Claraa ( Me )
-• Nurutomo
-• Adiwajshing
-• Xteam
-• Penyedia Layanan API
-• Orang-orang yang Berdonasi
+• Untuk Mu Yang Pernah Di Samping Ku :v
 ──────────────────────`
 await conn.sendButtonLoc(m.chat, await conn.resize('https://telegra.ph/file/0e265e801d892a340e877.png', 300, 300),  tqto, `Ini Ucapan Terima Kasih Untuk Orang Nya Telah Berpatisipasi
 
